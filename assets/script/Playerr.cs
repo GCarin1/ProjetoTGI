7d5890ab1a1f639703ee71ab6303dfba28f76626
Guilherme Carini <carinigcontact@gmail.com>
2021-04-13 19:05:14 -0300

    movimentação e animação implementadas

--- assets/script/Playerr.cs
+++ assets/script/Playerr.cs
@@ -1,24 +1,24 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
 public class Playerr : MonoBehaviour
 
 {
-
-        public float speed;
-        public Animator anim;
-
-    
+    public Animator anim;
+    public float speed;
+        
     void Update()
         {
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+
+        Vector3 movement= new Vector3 (Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0f);
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.magnitude);
 
-        transform.position = transform.position+movement*speed*Time.deltaTime;
+        transform.position = transform.position + movement * speed * Time.deltaTime;
     }
 
         
